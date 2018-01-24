320d71faa688c217f8e949c5b5218547adcf984b
Zeglinski, Lukasz <lukasz.zeglinski@mobica.com>
2018-01-24 11:10:05 +0100

    Add cleaning logcat before collecting logs

--- src/main/java/hudson/plugins/openstf/DeviceLogger.java
+++ src/main/java/hudson/plugins/openstf/DeviceLogger.java
@@ -4,9 +4,11 @@ import hudson.FilePath;
 import hudson.Proc;
 import hudson.plugins.android_emulator.sdk.Tool;
 import hudson.util.NullStream;
+import java.util.concurrent.TimeUnit;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.InterruptedException;
 
 public class DeviceLogger{
     public FilePath logcatFile;
@@ -14,10 +16,24 @@ public class DeviceLogger{
     public String logcatArgs;
     public Proc logWriter;
 
+    private void cleanupLogcatToCollectDataFromCurrentRun(AndroidRemoteContext remote, String remoteDeviceSerial){
+        try {
+            Proc clearLogcat = remote.getToolProcStarter(Tool.ADB, String.format("-s %s logcat -c", remoteDeviceSerial))
+                    .stdout(new NullStream()).stderr(new NullStream()).start();
+        } catch (IOException|InterruptedException ex)
+        {
+        //Ignore
+        }
+
+    }
+
     public DeviceLogger(AndroidRemoteContext remote, String remoteDeviceSerial, FilePath workspace){
+
+        cleanupLogcatToCollectDataFromCurrentRun(remote, remoteDeviceSerial);
+
         try{
             this.logcatFile = workspace.createTextTempFile("logcat_" + remoteDeviceSerial, ".log", "", false);
-        } catch (java.io.IOException|java.lang.InterruptedException ex) {
+        } catch (IOException|InterruptedException ex) {
             this.logcatFile = null;
         }
 
@@ -25,7 +41,7 @@ public class DeviceLogger{
             if (this.logcatFile != null){
                 this.logcatStream  = this.logcatFile.write();
             } else {this.logcatStream  = null;}
-        } catch (java.io.IOException|java.lang.InterruptedException ex) {
+        } catch (IOException|InterruptedException ex) {
             this.logcatStream = null;
         }
 
@@ -37,7 +53,7 @@ public class DeviceLogger{
                 .stdout(this.logcatStream).stderr(new NullStream()).start();
             }
             else{this.logWriter = null;}
-        } catch (java.io.IOException|java.lang.InterruptedException ex) {
+        } catch (IOException|InterruptedException ex) {
             this.logWriter = null;
         }
     }
